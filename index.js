64a8b6c19cdfef2859b84bb42c0f60531d9fd82b
Andrew Smiley <jayalfredprufrock@gmail.com>
2017-02-07 00:17:02 -0800

    Typo in umount package name.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var child_process = require('child_process');
-var os = require('os')
+var os = require('os');
 var path = require('path');
-var unmount = require('unmount');
+var umount = require('umount');
 
 exports.eject = function(drive, callback) {
 
 	if (os.platform() == 'win32'){
 
 		var exePath = path.join(__dirname, 'bin', 'EjectMedia.exe');
-		var params = [drive,'L'];
+		var params = [drive, '-L'];
 		var options = { timeout: 5000};
 
 		child_process.execFile(exePath, params, options, callback);
 	}
 	else {
 
-		ejectUnix(drive, callback);
+		umount.umount(drive, callback);
 	}
 
 };
